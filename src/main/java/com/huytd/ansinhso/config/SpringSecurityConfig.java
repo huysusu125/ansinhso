e8088cf2a13096d275013d9094cac9e958daf8a0
huytd <huysusu125@gmail.com>
2025-12-04 00:11:06 +0700

    fix cors

---
+++ src/main/java/com/huytd/ansinhso/config/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.huytd.ansinhso.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest()
+                        .permitAll()
+                )
+                .build();
+    }
+}
+
---
+++ src/main/java/com/huytd/ansinhso/filter/CorsFilter.java
@@ -0,0 +1,36 @@
+package com.huytd.ansinhso.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@Order(-1)
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        log.info("Request to URI :{}", uri);
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            resp.setHeader("Access-Control-Allow-Origin", "*");
+            resp.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            resp.setHeader("Access-Control-Max-Age", "3600");
+            resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        resp.setHeader("Access-Control-Max-Age", "3600");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        filterChain.doFilter(request, response);
+    }
+}
